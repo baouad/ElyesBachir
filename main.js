fb49206293909921ea20dcf41b324ea1c8c20efd
Elyes1290 <elyes.naitliman@gmail.com>
2022-04-29 10:56:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 console.log('Bonjour')
-//let cors = require("cors");
-//app.use(cors());
+// //let cors = require("cors");
+// //app.use(cors());
 
 async function getBeers(){
   try{
-      const response = await axios.get("http://entreprise.vinko-roditi.com/api/entreprise/list")
+      const response = await axios.get("https://api.punkapi.com/v2/beers")
       console.log("reponse", response);
-      let afficher = document.getElementById('afficher');
-      for (var i; i < afficher.length; i++)
-      {
-        afficher.innerHTML = arricher[i]; 
-      }
-           
+      
+    
+        for(let name of response.data){
+          console.log(name)
+        
+        
+        // create a new div element
+        var afficher = document.createElement("afficher");
+
+        // and give it some content
+        var newContent = document.createTextNode(name.name);
+
+        // add the text node to the newly created div
+        afficher.innerHTML = newContent;
+
+        // add the newly created element and its content into the DOM
+        document.body.insertBefore(afficher, newContent);
+}
+
   }catch (error){
       console.error(error);
   }
 }
 
 await getBeers()
-
