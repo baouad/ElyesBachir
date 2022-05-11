5ba3fb0e312680b397c1949f1de782c04498aa1c
Elyes1290 <99724750+Elyes1290@users.noreply.github.com>
2022-05-11 17:12:46 +0200

    last du last du last

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let card = document.getElementById("card")
-let idB;
 let nameB = ""
 let imageB = ""
 let taglineB = ""
@@ -16,56 +15,32 @@ async function getBeers() {
     //   myInput.focus()
     // })
     
-    for (const element of response.data) {
+    for (const element of response.data) {   
       nameB = element.name
       imageB = element.image_url
       taglineB = element.tagline
       description = element.description
+
       card.innerHTML += `
      
-      <div class="d-grid gap-12">
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target= #`+taglineB+`>
-      <p class="card-text" >`+taglineB+`
-      <img src="`+imageB+`" class="card-img-top" alt="..."></p>
+      <div class="container d-grid gap-10 col-12">
+      <button type="button" class="btn btn-lite" data-bs-toggle="modal" data-bs-target= #myModal>
+      <p class="card-text" >`+nameB+`
       </button>
+      <img src="`+imageB+`" class="card-img-top" alt="..." style="width: 18rem;"></p>
       <\div>
-    
-      
-      <!-- Modal -->
-      <div class="modal fade" id=`+taglineB+` tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            <div class="card" style="width: 18rem;">
-            <img src="`+imageB+`" class="card-img-top" alt="...">
-            <div class="`+nameB+`" id="name">
-              <p class="card-text" >`+taglineB+`</p>
-              <p class="des"> `+description+`</p>
-            </div>
-          </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-      
-   
-    `
+      ` 
     }
-
 
   } catch (error) {
   }
 }
 
 await getBeers()
+
+ 
+
+
 
 
 
